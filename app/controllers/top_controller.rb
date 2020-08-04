f0e6dba43cd1df01618b84b3e1e0f7d390d481a8
ueda-kenta <kosian.4141@gmail.com>
2020-08-04 11:17:11 +0000

    [Start]5-9

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,8 @@
+class TopController < ApplicationController
+
+	def top
+	end
+
+	def about
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'users/show'
+	root to: 'top#top'
+	get 'top/about' => 'top#about'
+	devise_for :users
+	resources :users, only: [:show]
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
