285d6d749744335f42f4907101996ebeeb32cb42
ueda-kenta <kosian.4141@gmail.com>
2020-08-05 11:40:22 +0000

    [Update]mypage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,9 +21,20 @@ class BooksController < ApplicationController
 	end
 
 	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		book = Book.find(params[:id])
+		book.update(book_params)
+		redirect_to book_path(book)
 	end
 
 	def destroy
+		book = Book.find(params[:id])
+		user = current_user
+		book.destroy
+		redirect_to user_path(user)
 	end
 
 	private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
     @book = Book.new
+    @books = Book.all
   end
 
   def create
